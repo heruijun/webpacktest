e898e45b700652a1bf8b657ad1bb502afcfd190c
heruijun <13585546884@163.com>
2018-04-28 14:35:18 +0800

    对象解构成数组

--- test1/src/page2.js
+++ test1/src/page2.js
@@ -2,3 +2,5 @@ import * as utils from 'utils'
 
 console.log(utils.test('测试2'))
 console.log(utils.numbers(1, 3, 5))
+console.log(utils.foo())
+console.log(utils.m2({ x: 2, y: 3}))
--- test1/src/utils.js
+++ test1/src/utils.js
@@ -6,3 +6,12 @@ export function test(text) {
 export const numbers = (...numbers) => {
     console.log(numbers)
 }
+
+// 当参数为一个对象时
+export function foo({ x , y = 5 } = {} ) {
+    console.log(x, y)
+}
+
+export function m2({ x, y } = { x : 1, y : 2 }) {
+    return [ x, y ]
+}
