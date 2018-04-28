c0a5d5249c7938c89e44b4d4e288e5e5e665b987
heruijun <13585546884@163.com>
2018-04-28 13:59:52 +0800

    rest

--- test1/src/page2.js
+++ test1/src/page2.js
@@ -1,3 +1,4 @@
 import * as utils from 'utils'
 
 console.log(utils.test('测试2'))
+console.log(utils.numbers(1, 3, 5))
--- test1/src/utils.js
+++ test1/src/utils.js
@@ -1,3 +1,8 @@
 export function test(text) {
     console.log(text)
 }
+
+// rest 参数（形式为...变量名），用于获取函数的多余参数
+export const numbers = (...numbers) => {
+    console.log(numbers)
+}
