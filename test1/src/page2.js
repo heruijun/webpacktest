2ab3fa0708226e9daac9ac3c7887af55d0d65538
heruijun <13585546884@163.com>
2018-05-04 16:47:13 +0800

    字符串模版

--- test1/src/page2.js
+++ test1/src/page2.js
@@ -5,8 +5,13 @@ console.log(utils.numbers(1, 3, 5))
 console.log(utils.foo())
 console.log(utils.m2({ x: 2, y: 3 }))
 
-const target = 'world'
-const word = `hello ${target}`  // 字符串模版
-console.log(word)
+// 字符串模版
+const name = 'Will Wen Gunn'
+const level = 'Gold'
+const message = `
+Hello, ${name}.
+Here is Turing Airline, you are the ${level} member of our Privilege Club.
+`
+console.log(message)
 
 console.log(utils.wordOnlyText('Hey dude, how is it going?'))
